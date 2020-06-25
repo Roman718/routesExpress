232ba476db96492eb6b57a7e31a88fcd2f1467fd
Romanx <mgkeze@gmail.com>
2020-06-26 02:04:52 +0600

    initial

---
+++ server/server.js
@@ -0,0 +1,110 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import sockjs from 'sockjs'
+import { renderToStaticNodeStream } from 'react-dom/server'
+import React from 'react'
+
+import cookieParser from 'cookie-parser'
+import config from './config'
+import Html from '../client/html'
+
+const Root = () => ''
+
+try {
+  // eslint-disable-next-line import/no-unresolved
+  // ;(async () => {
+  //   const items = await import('../dist/assets/js/root.bundle')
+  //   console.log(JSON.stringify(items))
+
+  //   Root = (props) => <items.Root {...props} />
+  //   console.log(JSON.stringify(items.Root))
+  // })()
+  console.log(Root)
+} catch (ex) {
+  console.log(' run yarn build:prod to enable ssr')
+}
+
+let connections = []
+
+const port = process.env.PORT || 8090
+const server = express()
+
+const middleware = [
+  cors(),
+  express.static(path.resolve(__dirname, '../dist/assets')),
+  bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
+  bodyParser.json({ limit: '50mb', extended: true }),
+  cookieParser()
+]
+
+middleware.forEach((it) => server.use(it))
+
+server.get('/api/v1/users', async (req, res) => {
+  const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users')
+  res.json(users)
+})
+
+// server.get('/api/v1/users/:id', async (req, res) => {
+//   const { id } = req.params
+//   const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+//   const user = data.filter(el => el.id === +id)
+//   res.json(user)
+// })
+
+server.get('/api/v1/users/:id', async (req, res) => {
+  const { id } = req.params
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+  const users = data.slice(0, id)
+  res.json(users)
+})
+
+server.use('/api/', (req, res) => {
+  res.status(404)
+  res.end()
+})
+
+const [htmlStart, htmlEnd] = Html({
+  body: 'separator',
+  title: 'Boilerplate'
+}).split('separator')
+
+server.get('/', (req, res) => {
+  const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
+  res.write(htmlStart)
+  appStream.pipe(res, { end: false })
+  appStream.on('end', () => {
+    res.write(htmlEnd)
+    res.end()
+  })
+})
+
+server.get('/*', (req, res) => {
+  const initialState = {
+    location: req.url
+  }
+
+  return res.send(
+    Html({
+      body: '',
+      initialState
+    })
+  )
+})
+
+const app = server.listen(port)
+
+if (config.isSocketsEnabled) {
+  const echo = sockjs.createServer()
+  echo.on('connection', (conn) => {
+    connections.push(conn)
+    conn.on('data', async () => {})
+
+    conn.on('close', () => {
+      connections = connections.filter((c) => c.readyState !== 3)
+    })
+  })
+  echo.installHandlers(app, { prefix: '/ws' })
+}
+console.log(`Serving at http://localhost:${port}`)
